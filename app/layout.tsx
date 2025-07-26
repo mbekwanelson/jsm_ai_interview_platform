7c34ec02e38fb40f0ff290776418cff0be4e059c
Ntsikelelo.Mbekwa <mbekwantsikelelonelson@gmail.com>
2025-07-26 20:58:26 +0200

    Setup Repo, Sign In, Sign Up, Auth, Routing and Dynamic form control

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
+import {Toaster} from "sonner";
 
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
@@ -9,7 +10,7 @@ const monaSans = Mona_Sans({
 
 export const metadata: Metadata = {
   title: "PrepWise",
-  description: "AN AI-powered platform for preparing mock interview",
+  description: "AN AI-powered platform for preparing for mock interview",
 };
 
 export default function RootLayout({
@@ -20,9 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${monaSans.className}  antialiased`}
+        className={`${monaSans.className} antialiased pattern`}
       >
+
         {children}
+
+      <Toaster/>
       </body>
     </html>
   );
